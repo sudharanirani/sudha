acee043b974cc4219fbc18d945ef0d2706b26107
sudha rani <sudharani.rani@globallogic.com>
2026-02-24 17:35:13 +0000

    debug comand added

--- testhello.py
+++ testhello.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def home():
     return "Hello World"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=true)
+    app.run(host="0.0.0.0", port=5001, debug=True)
