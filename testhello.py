f2fe33cbcb38c83d6caa2cad68c508f0b2c08d39
sudha rani <sudharani.rani@globallogic.com>
2026-03-02 19:12:20 +0000

    changed message

--- testhello.py
+++ testhello.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "Hello World"
+    return "Hello World Sudha"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
