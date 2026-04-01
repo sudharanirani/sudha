e5a889c26dc91042d7070af11943c8f4e326a1c3
sudha rani <sudharani.rani@globallogic.com>
2026-04-01 00:35:58 +0000

    new desc

--- myapp/testhello.py
+++ myapp/testhello.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "Hello World AI test"
+    return "Hello World AI testing new phase"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=50010, debug=True)
